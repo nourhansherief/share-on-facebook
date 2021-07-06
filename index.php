602dfca1ec45634f43c7c9a59e7946d36146eb86
Sherif <V19NSherif1@vodafone.com>
2021-07-07 00:06:34 +0200

    first commit

--- index.php
+++ index.php
@@ -17,16 +17,14 @@
     ?>
         <!-- You can use Open Graph tags to customize link previews.
         Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
-      <meta property="og:url"           content="<?php echo 'http://motorcarmyanmar.com' . $_SERVER['REQUEST_URI'];?>" />
+      <meta property="og:url"           content="<?php echo 'https://web.vodafone.com.eg/ar/home' ;?>" />
       <meta property="og:type"          content="website" />
-      <meta property="og:title"         content="<?php echo (!empty($title_header)?$title_header:'Some Buy,Sell,Rent');?>" />
-
-      <meta property="og:description"   content="<?php echo (!empty($description_header)?$description_header:'Car Sales,Car Rentals,Car Accessories Sales');?>" />
+       <meta property="og:title"         content="<?php echo (!empty($title_header)?$title_header:'I just voted to Amr Diab');?>" /> 
       <meta property="og:image"         content="<?php echo (!empty($image_header)?$image_header:'https://img.pngio.com/free-download-regular-polygon-shape-octadecagon-angle-round-png-round-png-260_260.jpg');?>" />
     <meta property="og:image:alt"    content="car picture"/>
-    <meta property="og:image:width" content="500" />
-    <meta property="og:image:height" content="400" />
-    <meta property="fb:app_id" content='328716478755626'>
+    <meta property="og:image:width" content="200" />
+    <meta property="og:image:height" content="200" />
+    <meta property="fb:app_id" content='838833720360017'>
     </head>
     <body>
     </body>
