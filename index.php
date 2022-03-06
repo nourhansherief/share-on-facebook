3a71db46af5a860d4358525000a3f8b22be4f6b4
nourhansherief <45966082+nourhansherief@users.noreply.github.com>
2022-03-06 12:56:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
         <!-- You can use Open Graph tags to customize link previews.
         Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
         <meta property="og:site_name" content="Vodafone Promo">
-      <meta property="og:url"           content="<?php echo 'https://web.vodafone.com.eg/ar/home' ;?>" />
       <meta property="og:type"          content="website" />
        <meta property="og:title"         content="<?php echo (!empty($title_header)?$title_header:'I just voted to Amr Diab');?>" /> 
       <meta property="og:image"         content="<?php echo (!empty($image_header)?$image_header:'https://img.pngio.com/free-download-regular-polygon-shape-octadecagon-angle-round-png-round-png-260_260.jpg');?>" />
@@ -29,8 +28,7 @@
     <meta property="og:image:width" content="200" />
     <meta property="og:image:height" content="200" />
     <meta property="fb:app_id" content='838833720360017'>
-    <meta property="og:link" content='https://web.vodafone.com.eg/ar/home'>
-    <meta property="og:href" content='https://web.vodafone.com.eg/ar/home'>
+ 
    
     </head>
     <body>
